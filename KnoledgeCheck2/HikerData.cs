c7f6b12bdc5345c2554b238777a546f47aaca5da
JMartindale2023@gmail.com <133904262+JMarty21@users.noreply.github.com>
2023-09-16 20:15:22 -0400

    Add files via upload

---
+++ KnoledgeCheck2/HikerData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeCheck2
+{
+    public class HikerData : HikingTrailAttributes
+    {
+        public string HikerFullName { get; set; }
+        
+        public string HikerFirstName { get; set; }
+        public string HikerLastName { get; set; }
+        
+        public DateOnly DateOfHike { get; set; }
+        public DateOnly HikerBirthDate { get; set; }
+
+        public override string ToString()
+        {
+            return "Hiker Name: " + HikerFullName + ".  Date of Hike: " + DateOfHike + ".  Trail Hiked: " + HikingTrailName;
+        }
+
+    }
+}
---
+++ KnoledgeCheck2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static KnowledgeCheck2.HikerData;
+
+namespace KnowledgeCheck2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many records do you want to add? ");
+            var numberOfRecords = int.Parse(Console.ReadLine());
+
+            var recordList = new List<HikerData>();
+            for (int i = 0; i < numberOfRecords; i++)
+            {
+                // In this loop, populate the object's properties using Console.ReadLine()
+                var HikerData = new HikerData();
+
+                Console.WriteLine("Enter the Hiker Name: ");
+                HikerData.HikerFullName = Console.ReadLine();
+                
+                Console.WriteLine("Enter the Date of Hike in format MM/DD/YYYY: ");
+                HikerData.DateOfHike = DateOnly.Parse(Console.ReadLine());
+                
+                Console.WriteLine("Enter the name of the Trail hiked: ");
+                HikerData.HikingTrailName = Console.ReadLine();
+                
+                recordList.Add(HikerData);
+                
+                Console.WriteLine("Hiker data added: " + HikerData.ToString());
+            }
+
+                if (recordList.Count > 0)
+            {
+                Console.WriteLine(" A total of " + numberOfRecords + " Hiker data records were added.");
+                foreach (var record in recordList)
+                {
+                    Console.WriteLine(record.ToString());
+                }
+            }
+            
+            // ^^Print out the list of records using Console.WriteLine() ^^
+        }
+    }
+}
